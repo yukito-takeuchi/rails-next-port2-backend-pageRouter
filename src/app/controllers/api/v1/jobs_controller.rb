b30ae093e80f720bdd1da03253b22060074b8667
yukito <iyggf66974@yahoo.ne.jp>
2025-04-23 11:40:17 +0900

    bcrypt　追加　成功

--- src/app/controllers/api/v1/jobs_controller.rb
+++ src/app/controllers/api/v1/jobs_controller.rb
@@ -1,27 +1,73 @@
 # app/controllers/api/v1/jobs_controller.rb
 class Api::V1::JobsController < ApplicationController
+  before_action :authorize_request, except: [:index, :show]
+  before_action :set_job, only: [:show, :update, :destroy]
+  before_action :require_company, only: [:create, :update, :destroy] # 企業ユーザーのみ許可
+
   def index
-    # 求人一覧を取得する処理を実装する
-    render json: { message: '求人一覧' }
+    @jobs = Job.all.map do |job|
+      user_icon_url = job.company.profile&.user_icon.attached? ? url_for(job.company.profile.user_icon) : nil
+      job_json = job.as_json.merge(
+        image_url: job.image.attached? ? url_for(job.image) : nil,
+        user_name: job.company.name,
+        user_icon_url: user_icon_url
+      )
+      job_json
+    end
+    render json: @jobs
   end
 
   def show
-    # 特定の求人を取得する処理を実装する
-    render json: { message: '求人詳細' }
+    user_icon_url = @job.company.profile&.user_icon.attached? ? url_for(@job.company.profile.user_icon) : nil
+    render json: @job.as_json.merge(
+      image_url: @job.image.attached? ? url_for(@job.image) : nil,
+      user_name: @job.company.name,
+      user_icon_url: user_icon_url
+    )
   end
 
+
   def create
-    # 求人を作成する処理を実装する
-    render json: { message: '求人作成' }
+    @job = current_user.jobs.build(job_params) # current_userは企業ユーザー
+    if @job.save
+      if params[:image]
+        @job.image.attach(params[:image])
+      end
+      render json: @job.as_json.merge(image_url: @job.image.attached? ? url_for(@job.image) : nil), status: :created
+    else
+      render json: { errors: @job.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
-    # 求人を更新する処理を実装する
-    render json: { message: '求人更新' }
+    if @job.update(job_params)
+      if params[:image]
+        @job.image.attach(params[:image])
+      end
+      render json: @job.as_json.merge(image_url: @job.image.attached? ? url_for(@job.image) : nil)
+    else
+      render json: { errors: @job.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    # 求人を削除する処理を実装する
-    render json: { message: '求人削除' }
+    @job.destroy
+    head :no_content
+  end
+
+  private
+
+  def job_params
+    params.permit(:title, :description, :location, :salary, :requirements, :benefits, :employment_type)
+  end
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def require_company
+    unless current_user.company?
+      render json: { error: '権限がありません。企業ユーザーのみ可能です。' }, status: :unauthorized
+    end
   end
 end
--- src/app/controllers/api/v1/users_controller.rb
+++ src/app/controllers/api/v1/users_controller.rb
@@ -1,32 +1,91 @@
-# app/controllers/api/v1/users_controller.rb
 class Api::V1::UsersController < ApplicationController
+  before_action :authorize_request, except: [:create, :show_by_id]
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def show_by_id
-    # IDでユーザーを取得する処理を実装する
-    render json: { message: 'ユーザー詳細 (ID指定)' }
+    @user = User.find_by(id: params[:id])
+    if @user
+      @profile = @user.profile
+      user_icon_url = nil
+      bg_image_url = nil
+  
+      if @profile
+        if @profile.user_icon&.attached?
+          user_icon_url = url_for(@profile.user_icon)
+        elsif @profile.respond_to?(:user_icon_url) && @profile.user_icon_url.present?
+          user_icon_url = @profile.user_icon_url
+        end
+  
+        if @profile.bg_image&.attached?
+          bg_image_url = url_for(@profile.bg_image)
+        elsif @profile.respond_to?(:bg_image_url) && @profile.bg_image_url.present?
+          bg_image_url = @profile.bg_image_url
+        end
+  
+        render json: {
+          id: @user.id,
+          name: @user.name,
+          user_type: @user.user_type,
+          profile: {
+            user_icon_url: user_icon_url,
+            bg_image_url: bg_image_url
+          }
+        }
+      else
+          render json: {
+            id: @user.id,
+            name: @user.name,
+            user_type: @user.user_type,
+            profile: nil
+          }
+      end
+    else
+      render json: { error: "User with id #{params[:id]} not found" }, status: :not_found
+    end
   end
 
   def index
-    # ユーザー一覧を取得する処理を実装する
-    render json: { message: 'ユーザー一覧' }
+    @users = User.all
+    render json: @users
   end
 
   def show
-    # 特定のユーザーを取得する処理を実装する
-    render json: { message: 'ユーザー詳細' }
+    render json: @user
   end
 
   def create
-    # ユーザーを作成する処理を実装する
-    render json: { message: 'ユーザー作成' }
+    @user = User.new(user_params)
+    if @user.save
+      # プロフィールを自動作成
+      @profile = @user.build_profile
+      @profile.save
+
+      render json: { user: { id: @user.id } }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
-    # ユーザーを更新する処理を実装する
-    render json: { message: 'ユーザー更新' }
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    # ユーザーを削除する処理を実装する
-    render json: { message: 'ユーザー削除' }
+    @user.destroy
+    head :no_content
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation, :user_type)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
